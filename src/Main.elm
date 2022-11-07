97df565e8fe94ed24fbe9771e518c8886b7fada5
jigargosar <jigar.gosar@gmail.com>
2022-11-07 13:51:41 +0530

    initializing board

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (main)
 import Dict exposing (Dict)
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (style)
+import Tuple exposing (pair)
 
 
 main : Html msg
@@ -25,7 +26,33 @@ type Board
 
 boardInit : Board
 boardInit =
-    Board Dict.empty
+    let
+        -- might be easier to visualize board by using LOL?
+        lol =
+            [ [ 0, 1, 0, 0 ]
+            , [ 0, 1, 1, 0 ]
+            ]
+
+        reducer2: Int -> (Int, Int) -> Dict Pos Int -> Dict Pos Int 
+        reducer2 y (x, val) acc = 
+            if val /= 0 then
+                Dict.insert (x,y) val acc
+            else
+                acc
+
+        reducer: (Int, List Int) -> Dict Pos Int -> Dict Pos Int 
+        reducer (y, ls) acc =
+            ls 
+                |> List.indexedMap pair 
+                |> List.foldl (reducer2 y) acc
+
+        dict =
+            lol
+                |> List.indexedMap pair
+                |> List.foldl reducer Dict.empty
+    in
+    -- Board (Dict.fromList [ ( pair 0 0, 1 ) ])
+    Board dict
 
 
 
