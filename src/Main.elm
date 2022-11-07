68ba6bfdbf93d3d71ac9d197fa2bc88bfe004ea7
jigargosar <jigar.gosar@gmail.com>
2022-11-07 17:21:20 +0530

    implementing move

--- src/Main.elm
+++ src/Main.elm
@@ -86,10 +86,10 @@ isValidBoardEntry ( x, y ) val =
 
 initialBoard : Board
 initialBoard =
-    [ [ 0, 0, 0, 0 ]
-    , [ 0, 1, 2, 0 ]
-    , [ 0, 3, 2, 0 ]
-    , [ 5, 0, 0, 7 ]
+    [ [ 0, 1, 0, 7 ]
+    , [ 0, 1, 3, 7 ]
+    , [ 0, 2, 3, 7 ]
+    , [ 5, 2, 0, 7 ]
     ]
         |> boardFromLists
 
