08b636cf0dfc3563a5eab584d86cd32d45662232
jigargosar <jigar.gosar@gmail.com>
2022-11-13 12:01:33 +0530

    implementing core game Logic.elm.

--- tests/LogicTests.elm
+++ tests/LogicTests.elm
@@ -1,19 +1,28 @@
 module LogicTests exposing (..)
 
 import Expect
+import Fuzz
 import Logic
-import Random
 import Test exposing (Test)
+
+
+fuzzBoard =
+    Fuzz.fromGenerator Logic.randomBoard
 
 
 suite : Test
 suite =
     Test.describe "Initial random Board"
-        [ Test.test "should contain 2 entries" <|
-            \_ ->
-                Random.step Logic.randomBoard (Random.initialSeed 0)
-                    |> Tuple.first
+        [ Test.fuzz fuzzBoard
+            "should have exactly 2 values each equal to either 2 or 4"
+          <|
+            \board ->
+                board
                     |> Logic.toList
+                    |> List.filter
+                        (Logic.entryVal
+                            >> (\val -> val == 2 || val == 4)
+                        )
                     |> List.length
                     |> Expect.equal 2
         ]
