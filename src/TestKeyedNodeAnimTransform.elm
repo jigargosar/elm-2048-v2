73a21b754f2a49e72a445db35780a2f560503bce
jigargosar <jigar.gosar@gmail.com>
2022-11-14 15:26:59 +0530

    TestKeyedNodeAnimTransform.elm

--- src/TestKeyedNodeAnimTransform.elm
+++ src/TestKeyedNodeAnimTransform.elm
@@ -1,7 +1,11 @@
 module TestKeyedNodeAnimTransform exposing (main)
 
 import Browser
+import Css exposing (backgroundColor, color, column, display, displayFlex, flexDirection, fontFamily, fontSize, height, hsl, minHeight, monospace, padding, pct, property, px, rgb, row, vh)
+import Css.Global as Global
+import Css.Media exposing (grid)
 import Html.Styled exposing (div, text)
+import Html.Styled.Attributes exposing (css)
 
 
 main =
@@ -13,7 +17,7 @@ main =
 
 
 init =
-    {}
+    { list = List.range 1 10 |> List.map String.fromInt }
 
 
 update msg model =
@@ -22,4 +26,32 @@ update msg model =
 
 view model =
     Html.Styled.toUnstyled <|
-        div [] [ text "HW" ]
+        div
+            [ css
+                [ padding (px 20)
+                ]
+            ]
+            [ text "HW"
+            , Global.global
+                [ Global.body
+                    [ displayGrid
+                    , h100
+                    , fontSize (px 20)
+                    , fontFamily monospace
+                    , backgroundColor (hsl 0 0 0.2)
+                    , color (hsl 0 0 0.8)
+                    ]
+                ]
+            ]
+
+
+displayGrid =
+    property "display" "grid"
+
+
+displayFlexColumn =
+    Css.batch [ displayFlex, flexDirection column ]
+
+
+h100 =
+    height (pct 100)
